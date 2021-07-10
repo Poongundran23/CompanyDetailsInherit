0eb6a6de939177ec2819b708313806b0ff3f1d23
Poongundran23 <poongundran27@gmail.com>
2021-07-10 21:10:06 +0530

    updated

--- src/org/company/Company.java
+++ src/org/company/Company.java
@@ -3,6 +3,7 @@ import org.client.*;
 public class Company extends Client {
 	private void companyName() {
 		System.out.println("Company Name:Greens");
+		System.out.println("Company Name:Greens");
 	}
 	public static void main(String[] args) {
 		Company c = new Company();
